70ed25a4e73ee4daade14b81f8a7946a3c10dbf2
hernanquerbes <hernanqd@uw.edu>
2025-11-05 10:01:48 -0800

    Refactor visualization code to save multiple plots
    Refactor code to iterate over results and save plots for each key. Removed hardcoded key references and added dynamic output file naming.

--- visualize_2.py
+++ visualize_2.py
@@ -5,26 +5,31 @@ import pickle
 import sys
 
 run_dir = Path(sys.argv[1])
-output = run_dir / "test" / "model_epoch003" / "test_timeseries.png"
+# output = run_dir / "test" / "model_epoch003" / "test_timeseries.png"
 
 with open(run_dir / "test" / "model_epoch003" / "test_results.p", "rb") as fp:
     results = pickle.load(fp)
 
-results.keys()
+for key in results.keys():
+    # extract observations and simulations
+    DS = results[key]['1D']['xr']
+    print(DS)
+    qobs = DS['QObs(mm/d)_obs']
+    qsim = DS['QObs(mm/d)_sim']
+    
+    fig, ax = plt.subplots(figsize=(16,10))
+    ax.plot(qobs['date'], qobs, label='Observed')
+    ax.plot(qsim['date'], qsim, label='Simulated')
+    ax.set_ylabel("Discharge (mm/d)")
+    ax.set_title(f"Test period - NSE {results[key]['1D']['NSE']:.3f}")
+    ax.legend()
+    fig.tight_layout()
 
-# extract observations and simulations
-DS = results['01022500']['1D']['xr']
-qobs = DS['QObs(mm/d)_obs']
-qsim = DS['QObs(mm/d)_sim']
+    output = run_dir / "test" / "model_epoch003" / f"{key}_test_timeseries.png"
+    plt.savefig(output)
+    plt.close(fig)
+    print(f"Saved plot to {output}")
 
-fig, ax = plt.subplots(figsize=(16,10))
-ax.plot(qobs['date'], qobs)
-ax.plot(qsim['date'], qsim)
-ax.set_ylabel("Discharge (mm/d)")
-ax.set_title(f"Test period - NSE {results['01022500']['1D']['NSE']:.3f}")
-plt.savefig(output)
-print(f"Saved plot to {output}")
-
-values = metrics.calculate_all_metrics(qobs.isel(time_step=-1), qsim.isel(time_step=-1))
-for key, val in values.items():
-    print(f"{key}: {val:.3f}")
+# values = metrics.calculate_all_metrics(qobs.isel(time_step=-1), qsim.isel(time_step=-1))
+# for key, val in values.items():
+#     print(f"{key}: {val:.3f}")
